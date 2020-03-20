cf3035347598abd354e8e01f4fde3be0accdcee3
Dhuha <samsudcoba@gmail.com>
2020-03-20 22:11:26 +0700

    update latian

---
+++ app/src/main/java/com/example/polar/view/latihan/PetunjukLatihan.kt
@@ -0,0 +1,13 @@
+package com.example.polar.view.latihan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.polar.R
+
+class PetunjukLatihan : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_petunjuk_latihan)
+    }
+}
