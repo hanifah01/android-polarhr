c5eb25db5abe00a48d73be3ee573667707feee6c
hanifah01 <hanifa.mustika@gmail.com>
2020-06-05 23:28:33 +0700

    nambah alert dialog jika latihan sudah 5 kali

---
+++ app/src/main/java/com/example/polar/view/pelatih/HomePelatih.kt
@@ -0,0 +1,12 @@
+package com.example.polar.view.pelatih
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomePelatih : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_pelatih)
+    }
+}
